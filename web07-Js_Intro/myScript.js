9fd2e7e822f330dbbdabf06e1f7c63f357207b8b
carlosMtz <carmazs19@gmail.com>
2025-05-12 16:01:29 -0600

    feat:se agregaron trabajos

---
+++ web07-Js_Intro/myScript.js
@@ -0,0 +1,33 @@
+
+function saludar() {
+    alert("Ya Casi Nos Vamos Al Primer Descanso");
+}
+
+function bodyColor( color ){
+    document.body.style.backgroundColor = color;
+}
+function colorDeFondo (){
+    document.body.style.backgroundColor = "black"
+}
+function changeToGreenColor(){
+    const element =document.getElementById("green-paragraph");
+    console.log(element);
+    element.style.color= "green";
+}
+function changeColor(element, color){
+    element.style.color=color;
+}
+
+(function setUp(){
+    document.getElementById("title").style.color = "blue";
+    document.getElementById("title").style.fontSize = "50px";
+
+})();
+
+function saludillo(){
+    alert("Te la cresite we jsjsjsjjs")
+}
+
+function cambiarNombre(){
+
+}
